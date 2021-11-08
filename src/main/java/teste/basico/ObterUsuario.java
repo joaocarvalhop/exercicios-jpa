c208b1733e993386190177db5cd771ee00f33287
joaocarvalhop <jppcarvalho8@gmail.com>
2021-11-08 12:41:08 -0300

    Obter Usuário

--- src/main/java/teste/basico/ObterUsuario.java
+++ src/main/java/teste/basico/ObterUsuario.java
@@ -3,9 +3,9 @@ package teste.basico;
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
-
 import modelo.basico.Usuario;
 
+// Select
 public class ObterUsuario {
 
 	public static void main(String[] args) {
@@ -13,10 +13,11 @@ public class ObterUsuario {
 		EntityManagerFactory emf = Persistence.createEntityManagerFactory("exercicios-jpa");
 		EntityManager em = emf.createEntityManager();
 
+		// o Select não precisa do Transaction
 		// L tipo long
 		Usuario user = em.find(Usuario.class, 3L);
 		System.out.println(user.getNome());
-		
+
 		em.close();
 		emf.close();
 	}
