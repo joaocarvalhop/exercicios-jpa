8d60b70a861d72a11b7d198365962176d7bece35
joaocarvalhop <jppcarvalho8@gmail.com>
2021-11-17 10:02:54 -0300

    Novo Produto

---
+++ src/main/java/teste/basico/NovoProduto.java
@@ -0,0 +1,19 @@
+package teste.basico;
+
+import infra.DAO;
+import modelo.basico.Produto;
+
+public class NovoProduto {
+
+	public static void main(String[] args) {
+
+		Produto p1 = new Produto("Notebook", 2988.89);
+		DAO<Produto> dao = new DAO<>(Produto.class);
+		// abrir transaçao incluir produto e fechar transação
+		// dao.abrirT().incluir(p1).fechar();
+		dao.incluirAtomico(p1).fechar();
+		
+		System.out.println("Id do produto inserido: " + p1.getId());
+	}
+
+}
