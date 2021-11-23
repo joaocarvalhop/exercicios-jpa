f462d18d48965ea8dbf19f24f7f8c8e78a8de0b1
joaocarvalhop <86267920+joaocarvalhop@users.noreply.github.com>
2021-11-23 15:13:11 -0300

    Um Pra Muitos #02

--- src/main/java/modelo/umpramuitos/Pedido.java
+++ src/main/java/modelo/umpramuitos/Pedido.java
@@ -1,12 +1,14 @@
 package modelo.umpramuitos;
 
 import java.util.Date;
+import java.util.List;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
 
 @Entity
 public class Pedido {
@@ -17,6 +19,11 @@ public class Pedido {
 
 	@Column(nullable = false)
 	private Date data;
+
+	// mappedBy = qual é o atributo dentro da classe ItemPedido que mapea o
+	// relacionamento, e é sempre colocado no lado bidirecional da relação
+	@OneToMany(mappedBy = "pedido")
+	private List<ItemPedido> itens;
 
 	public Pedido() {
 		// passa a data atual
@@ -42,6 +49,14 @@ public class Pedido {
 
 	public void setData(Date data) {
 		this.data = data;
+	}
+
+	public List<ItemPedido> getItens() {
+		return itens;
+	}
+
+	public void setItens(List<ItemPedido> itens) {
+		this.itens = itens;
 	}
 
 }
